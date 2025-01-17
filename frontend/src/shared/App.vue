bc941e5a7fd4df361710a23cdf64c890d8dcfe23
Mike Barlozewski <108719328+BarloGER@users.noreply.github.com>
2025-01-17 17:51:53 +0100

    feat: add vue router and basic routing

---
+++ frontend/src/shared/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style scoped>
+header {
+  height: 50px;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: large;
+  margin-right: 1rem;
+  gap: 1rem;
+}
+
+router-link {
+  text-decoration: none;
+  margin-right: 1rem;
+}
+</style>
---
+++ frontend/src/shared/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { router } from "./shared/routes/router"
+import {createI18n} from 'vue-i18n';
+// TODO: Error beseitigen
+import en from './shared/locales/en.json'
+import de from './shared/locales/de.json'
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'de',
+    fallbackLocale: 'en',
+    messages: {
+        en,
+        de
+    }
+})
+
+createApp(App).use(i18n).use(router).mount('#app')
